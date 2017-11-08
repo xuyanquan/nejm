a3ea9a45174dccab36f6b3b5ff1e8187f4d2ed26
lleohao <lleohao@hotmail.com>
2017-11-08 10:32:03 +0800

    add regularModule

--- template/NEJ.INDEX.js
+++ template/NEJ.INDEX.js
@@ -1,36 +1,37 @@
-import chain from './base/chain.js';
-import config from './base/config.js';
-import constant from './base/constant.js';
-import element from './base/element.js';
-import event from './base/event.js';
-import global from './base/global.js';
-import klass from './base/klass.js';
-import platform from './base/platform.js';
-import util from './base/util.js';
-import ui from './ui/base.js';
-import uiMaskMask from './ui/mask/mask.js';
-import utilAjaxTag from './util/ajax/tag.js';
-import utilAjaxXdr from './util/ajax/xdr.js';
-import utilAnimationEaseinout from './util/animation/easeinout.js';
-import utilCacheCache from './util/cache/cache.js';
-import utilCacheCookie from './util/cache/cookie.js';
-import utilChainNodeList from './util/chain/NodeList.js';
-import utilChainChainable from './util/chain/chainable.js';
-import utilEditorCommand from './util/editor/command.js';
-import utilEditorCommandSimple from './util/editor/command/simple.js';
-import utilEncodeJson from './util/encode/json.js';
-import utilEvent from './util/event.js';
-import utilEventEvent from './util/event/event.js';
-import utilFileSelect from './util/file/select.js';
-import utilFlashFlash from './util/flash/flash.js';
-import utilFormForm from './util/form/form.js';
-import utilHistoryHistory from './util/history/history.js';
-import utilLazyImage from './util/lazy/image.js';
-import utilSuggestSuggest from './util/suggest/suggest.js';
-import utilTabTab from './util/tab/tab.js';
-import utilTemplateJst from './util/template/jst.js';
-import utilTemplateTpl from './util/template/tpl.js';
-import utilTimerAnimation from './util/timer/animation.js';
+import chain from './base/chain';
+import config from './base/config';
+import constant from './base/constant';
+import element from './base/element';
+import event from './base/event';
+import global from './base/global';
+import klass from './base/klass';
+import platform from './base/platform';
+import util from './base/util';
+import ui from './ui/base';
+import uiMaskMask from './ui/mask/mask';
+import utilAjaxTag from './util/ajax/tag';
+import utilAjaxXdr from './util/ajax/xdr';
+import utilAnimationEaseinout from './util/animation/easeinout';
+import utilCacheCache from './util/cache/cache';
+import utilCacheCookie from './util/cache/cookie';
+import utilChainNodeList from './util/chain/NodeList';
+import utilChainChainable from './util/chain/chainable';
+import utilEditorCommand from './util/editor/command';
+import utilEditorCommandSimple from './util/editor/command/simple';
+import utilEncodeJson from './util/encode/json';
+import utilEvent from './util/event';
+import utilEventEvent from './util/event/event';
+import utilFileSelect from './util/file/select';
+import utilFlashFlash from './util/flash/flash';
+import utilFormForm from './util/form/form';
+import utilHistoryHistory from './util/history/history';
+import utilLazyImage from './util/lazy/image';
+import utilRegularModule from './util/dispatcher/regularModule';
+import utilSuggestSuggest from './util/suggest/suggest';
+import utilTabTab from './util/tab/tab';
+import utilTemplateJst from './util/template/jst';
+import utilTemplateTpl from './util/template/tpl';
+import utilTimerAnimation from './util/timer/animation';
 
 export {
     chain,
@@ -61,6 +62,7 @@ export {
     utilFormForm,
     utilHistoryHistory,
     utilLazyImage,
+    utilRegularModule,
     utilSuggestSuggest,
     utilTabTab,
     utilTemplateJst,
