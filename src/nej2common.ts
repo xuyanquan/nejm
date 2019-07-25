a9ed43e9966d7d52c08c784eff24d19d56be59f4
lleohao <lleohao@hotmail.com>
2019-07-25 18:47:53 +0800

    refactor: 使用 exports 变量导出 nej 模块

--- src/nej2common.ts
+++ src/nej2common.ts
@@ -1,12 +1,13 @@
 import template from '@babel/template';
 import { NodePath } from '@babel/traverse';
 import * as types from '@babel/types';
-import { IfStatement, Program } from '@babel/types';
+import { IfStatement, Program, Statement } from '@babel/types';
 import {
-    generatorNejDependenciesAsCommonjs,
+    generatorNejDependencies,
     nejCodeParser,
     generatorNejInjects,
-    transformReturnToModuleExports, transformThis2Window
+    transformReturnToExport,
+    transformThis2Window, generatorNejDependenciesAsCommonjs, transformReturnToModuleExports, NejInjectType
 } from 'babel-helper-nej-transforms';
 import * as path from 'path';
 import { transformNejDepPath } from './util';
@@ -22,7 +23,9 @@ export default function () {
         visitor: {
             Program: {
                 exit: (program: NodePath<Program>, options) => {
-                    const {
+                    options.isNejCode = true;
+
+                    let {
                         fnBody, dependencies, nejInject
                     } = nejCodeParser(program, options);
 
@@ -35,13 +38,56 @@ export default function () {
                         fileName = './' + path.relative(nejRoot, filename);
                     }
 
+                    if (!nejInject) {
+                        console.log(filename);
+                    }
+
                     dependencies.forEach(dep => {
                         dep.source = transformNejDepPath(dep.rawSource, fileName);
                     });
 
                     const IMPORT_LIST = generatorNejDependenciesAsCommonjs(dependencies);
-                    const NEJ_INJECT = generatorNejInjects(nejInject);
-                    const FN_BODY = transformThis2Window(transformReturnToModuleExports(fnBody, nejInject));
+
+                    const NEJ_INJECT = nejInject.map((inject, index) => {
+                        let statement: Statement;
+                        const {type, alias} = inject;
+                        switch (type) {
+                            case NejInjectType.array:
+                                statement = types.variableDeclaration('var', [
+                                    types.variableDeclarator(types.identifier(alias), types.arrayExpression())
+                                ]);
+                                break;
+                            case NejInjectType.function:
+                                statement = types.variableDeclaration('var', [
+                                    types.variableDeclarator(types.identifier(alias), types.functionExpression(null, [], types.blockStatement([])))
+                                ]);
+                                break;
+                            case NejInjectType.object:
+                                if (index === 0) { // 第一个注入变量通常导出
+                                    statement = types.variableDeclaration('var', [
+                                        types.variableDeclarator(types.identifier(alias), types.identifier('exports'))
+                                    ]);
+                                } else {
+                                    statement = types.variableDeclaration('var', [
+                                        types.variableDeclarator(types.identifier(alias), types.objectExpression([]))
+                                    ]);
+                                }
+
+                                break;
+                            default:
+                                statement = types.variableDeclaration('var', [
+                                    types.variableDeclarator(types.identifier(alias))
+                                ]);
+                        }
+
+                        return statement;
+                    });
+
+                    if (types.isReturnStatement(fnBody[fnBody.length - 1])) {
+                        fnBody = fnBody.slice(0, fnBody.length - 1);
+                    }
+
+                    const FN_BODY = transformThis2Window(fnBody);
 
                     program.node.body = buildWrapper({
                         IMPORT_LIST,
