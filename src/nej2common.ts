b6060de7ce34788803adc8e8d15d82b12de6b370
lleohao <lleohao@hotmail.com>
2019-07-25 16:15:48 +0800

    refactor: 使用 babel-helper-nej-transforms 重构

--- src/nej2common.ts
+++ src/nej2common.ts
@@ -1,17 +1,19 @@
 import template from '@babel/template';
 import { NodePath } from '@babel/traverse';
 import * as types from '@babel/types';
-import { IfStatement, Program, Statement } from '@babel/types';
+import { IfStatement, Program } from '@babel/types';
+import {
+    generatorNejDependenciesAsCommonjs,
+    nejCodeParser,
+    generatorNejInjects,
+    transformReturnToModuleExports, transformThis2Window
+} from 'babel-helper-nej-transforms';
 import * as path from 'path';
 import { transformNejDepPath } from './util';
-import { NejInjectType } from './util/interfaces/nej-inject.interface';
-import { nejParser } from './util/nej-parser';
-import { transformReturnToExport } from './util/transform-return-to-export';
-import { transformThis2Window } from './util/transform-this-to-window';
 
 const buildWrapper = template.statements(`
     IMPORT_LIST
-    NEJ_INJECT_LIST
+    NEJ_INJECT
     FN_BODY
 `);
 
@@ -21,61 +23,29 @@ export default function () {
             Program: {
                 exit: (program: NodePath<Program>, options) => {
                     const {
-                        fnBody, nejInject, dependence
-                    } = nejParser(program);
-                    let {opts: {fileName, nejRoot}, filename} = options;
+                        fnBody, dependencies, nejInject
+                    } = nejCodeParser(program, options);
 
-                    if (fnBody === undefined) {
+                    if (!fnBody) {
                         return;
                     }
 
+                    let {opts: {fileName, nejRoot}, filename} = options;
                     if (filename && nejInject) {
                         fileName = './' + path.relative(nejRoot, filename);
                     }
 
-                    const IMPORT_LIST = [];
-                    const NEJ_INJECT_LIST = [];
-                    const FN_BODY = transformReturnToExport(transformThis2Window(fnBody), nejInject);
-
-                    dependence.forEach(({dep, alias}) => {
-                        IMPORT_LIST.push(types.variableDeclaration('const', [
-                            types.variableDeclarator(
-                                types.identifier(alias),
-                                types.callExpression(types.identifier('require'), [types.stringLiteral(transformNejDepPath(dep, fileName))])
-                            )
-                        ]))
+                    dependencies.forEach(dep => {
+                        dep.source = transformNejDepPath(dep.rawSource, fileName);
                     });
 
-                    nejInject.forEach(({type, alias}) => {
-                        let statement: Statement;
-                        switch (type) {
-                            case NejInjectType.array:
-                                statement = types.variableDeclaration('var', [
-                                    types.variableDeclarator(types.identifier(alias), types.arrayExpression())
-                                ]);
-                                break;
-                            case NejInjectType.function:
-                                statement = types.variableDeclaration('var', [
-                                    types.variableDeclarator(types.identifier(alias), types.functionExpression(null, [], types.blockStatement([])))
-                                ]);
-                                break;
-                            case NejInjectType.object:
-                                statement = types.variableDeclaration('var', [
-                                    types.variableDeclarator(types.identifier(alias), types.objectExpression([]))
-                                ]);
-                                break;
-                            default:
-                                statement = types.variableDeclaration('var', [
-                                    types.variableDeclarator(types.identifier(alias))
-                                ]);
-                        }
-
-                        NEJ_INJECT_LIST.push(statement);
-                    });
+                    const IMPORT_LIST = generatorNejDependenciesAsCommonjs(dependencies);
+                    const NEJ_INJECT = generatorNejInjects(nejInject);
+                    const FN_BODY = transformThis2Window(transformReturnToModuleExports(fnBody, nejInject));
 
                     program.node.body = buildWrapper({
                         IMPORT_LIST,
-                        NEJ_INJECT_LIST,
+                        NEJ_INJECT,
                         FN_BODY
                     });
                 }
--- src/util/interfaces/nej-meta.interface.ts
+++
@@ -1,9 +0,0 @@
-import { Statement } from '@babel/types';
-import { DependenceInfo } from './dependence-info.interface';
-import { NejInject } from './nej-inject.interface';
-
-export interface NejMeta {
-    fnBody: Statement[];
-    dependence: DependenceInfo[];
-    nejInject: NejInject[];
-}
